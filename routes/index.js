5b9ccd75535fbf150288d5cd1a40ce6ecdd00b87
arnont <nongnont.170442@gmail.com>
2021-10-22 05:22:50 +0700

    test get data student

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const db = require('./queires');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 router.get('/student',db.getStudents);
 
 module.exports = router;
--- routes/queires.js
+++ routes/queires.js
@@ -1,5 +1,5 @@
 const { request } = require('express');
-const moment = require('moment');
+// const moment = require('moment');
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL = 'postgresql://emxscnsfesbxcy:911fd3b39c19e7b30c94b0965174e752c08686a2b66d5b3e77113c769aee2665@ec2-23-22-243-103.compute-1.amazonaws.com:5432/ddi7pvl5ctf0lr',
@@ -15,7 +15,7 @@ let client = null;
 
 const getStudents = async (req, res) => {
     try {
-      const result = await client.query('SELECT student_id, firstname, lastname, dob, sex, email, epigram, status, education_status, graduate_year, major_id, public_relation_id, image_profile FROM pro.student');
+      const result = await client.query(`SELECT student_id, firstname, lastname, dob, sex, email, epigram, status, education_status, graduate_year, major_id, public_relation_id, image_profile FROM student`);
       const results = { 'results': (result) ? result.rows : null };
       res.json(results);
       client.release();

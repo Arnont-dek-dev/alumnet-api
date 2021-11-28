6e6489529c9af8f3ddc8fd3d7de39085b9183681
arnont <nongnont.170442@gmail.com>
2021-11-28 14:51:08 +0700

    -

--- routes/queries.js
+++ routes/queries.js
@@ -1,5 +1,6 @@
 const { request } = require('express');
 const moment = require('moment');
+const {google} = require('googleapis');
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL = 'postgresql://emxscnsfesbxcy:911fd3b39c19e7b30c94b0965174e752c08686a2b66d5b3e77113c769aee2665@ec2-23-22-243-103.compute-1.amazonaws.com:5432/ddi7pvl5ctf0lr',
@@ -1175,7 +1176,7 @@ const deletegooglesheet=  async (req,res) => {
 // 2. Install the Node.js client library by running
 //    `npm install googleapis --save`
 
-const {google} = require('googleapis');
+
 const sheets = google.sheets('v4');
 
 async function main () {
